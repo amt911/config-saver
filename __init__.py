6f0c37b7098b04580e85c21da4f90a2cc70cee82
Andrés Merlo Trujillo <a.merlo.truji10@gmail.com>
2025-10-14 13:00:42 +0200

    Created version for my project

---
+++ __init__.py
@@ -0,0 +1,10 @@
+__version__ = "1.0.0"
+
+"""
+For versioning, the best practice is Semantic Versioning (SemVer):
+Format: MAJOR.MINOR.PATCH
+
+MAJOR: Breaking changes
+MINOR: New features, backward compatible
+PATCH: Bug fixes, backward compatible
+"""
